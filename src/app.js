bfab6127f65821b31dcbe87e47fda99ec67fad3b
audreyokamura <audreyqc@gmail.com>
2022-02-01 17:48:03 +0000

    Created functions to compare the selected pair of cards

--- src/app.js
+++ src/app.js
@@ -31,7 +31,36 @@ function displayCards() {
 }
 
 function flip() {
+  let flipedCards = document.querySelectorAll(".fliped:not(.right)");
+  if (flipedCards.length > 1) {
+    return;
+  }
   this.classList.add("fliped");
+  flipedCards = document.querySelectorAll(".fliped:not(.right)");
+  if (flipedCards.length < 2) {
+    return;
+  }
+  compare(flipedCards);
+}
+
+function compare(cardPairs) {
+  if (cardPairs[0].dataset.value === cardPairs[1].dataset.value) {
+    correct(cardPairs);
+  } else {
+    incorrect(cardPairs);
+  }
+}
+
+function correct(cardPairs) {
+  cardPairs.forEach(function (element) {
+    element.classList.add("right");
+  });
+}
+
+function incorrect(cardPairs) {
+  cardPairs.forEach(function (element) {
+    element.classList.remove("fliped");
+  });
 }
 
 displayCards();
