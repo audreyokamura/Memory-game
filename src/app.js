f4ba9262560c2532b6be7e14c278d6514336aeb6
audreyokamura <audreyqc@gmail.com>
2022-02-01 16:21:29 +0000

    Added function to shuffle the cards before display

--- src/app.js
+++ src/app.js
@@ -13,9 +13,17 @@ let cards = [
 
 let setCards = cards.concat(cards);
 
+function shuffleCards() {
+  let result = setCards.sort(function () {
+    return 0.5 - Math.random();
+  });
+  return result;
+}
+
 function displayCards() {
   let table = document.querySelector("#table");
-  setCards.forEach(function (element) {
+  let shuffledCards = shuffleCards();
+  shuffledCards.forEach(function (element) {
     let card = document.createElement("div");
     card.innerHTML = `<div class='card' data-value= ${element}><div class='card-content'>${element}</div></div>`;
     table.appendChild(card);
