d28e63d34b4ca2bb8ab4d83e13e8d14405fd300b
audreyokamura <audreyqc@gmail.com>
2022-02-01 14:45:25 +0000

    Created function to display the cards

--- src/app.js
+++ src/app.js
@@ -13,4 +13,14 @@ let cards = [
 
 let setCards = cards.concat(cards);
 
-console.log(setCards);
+function displayCards() {
+  let table = document.querySelector("#table");
+  setCards.forEach(function (element) {
+    let card = document.createElement("div");
+    card.innerHTML = `<div class='card' data-value= ${element}><div class='card-content'>${element}</div></div>`;
+    table.appendChild(card);
+  });
+  console.log(table);
+}
+
+displayCards();
